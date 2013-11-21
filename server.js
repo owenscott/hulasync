06ef5eabeeb719789923b0703d0000cdbed8e824
Abhishek Kapatkar <cabhishek@gmail.com>
2013-11-20 20:40:37 -0500

    add a template engine swig

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var express = require('express'),
+    swig = require('swig'),
+    path = require('path'),
     app = express();
 
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('view cache', false);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
 
 app.get('/', function(req, res){
   var body = 'Hello World';
