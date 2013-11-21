285aff9a13fdc39dd1e0a62158dd476429a3b912
Abhishek Kapatkar <cabhishek@gmail.com>
2013-11-21 12:55:02 -0500

    express settings

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 var express = require('express'),
+    swig = require('swig'),
+    path = require('path'),
     app = express();
+
+
+//Express settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('view cache', false);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+    swig.setDefaults({
+        cache: false
+    });
+}
 
 
 app.get('/', function(req, res){
