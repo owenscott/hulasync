375db18dd385cf66231cc275e21784e2a4c5ad71
Owen Scott <owen.m.scott@gmail.com>
2013-12-17 23:53:01 -0500

    build process and cleanup

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+var grunt = require('grunt');
+
+module.exports = function(grunt) {
+	
+	//Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      src: ['Gruntfile.js', 'server.js', 'public/js/app.js', 'public/js/**/*.js'],
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        smarttabs: true,
+        globals: {
+          require: true,
+          define: true,
+          requirejs: true,
+          describe: true,
+          expect: true,
+          it: true
+        },
+        node: true
+      }
+    },
+    
+    browserify: {
+			dist: {
+				files: {
+				  'public/bundle.js': ['public/js/app.js', 'public/js/**/*.js'],
+				},
+				options: {
+				  //transform: ['uglifyjs']
+				}
+			}
+		}
+  });
+};
+	
+	
+	
+  // Load JSHint task
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task.
+  grunt.registerTask('default', 'jshint');
+  
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-browserify');
+
